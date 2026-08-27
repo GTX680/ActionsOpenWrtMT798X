17b59f53c464e6c0ecb1161d3bec90f0a20c9bf9
jameslau-tech <Jameslau@126.com>
2026-08-27 14:50:05 +0800

    Update diy-part2.sh

--- 25.12/diy-part2.sh
+++ 25.12/diy-part2.sh
@@ -117,6 +117,7 @@ cat <<EOF > files/etc/docker/daemon.json
 }
 EOF
 
+mkdir -p files/etc/sysctl.d
 cat > files/etc/sysctl.d/99-mt7986a-optimize.conf << 'SYSCTL'
 # --- 1. 队列与拥塞控制 (低延迟核心) ---
 # 使用 fq_codel 队列，这是目前降低延迟的神器
